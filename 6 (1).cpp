12a2f85022a7022821d40063f103af5f928b01ab
Sowndhar <a.sowndhar3@gmail.com>
2023-12-28 20:49:17 +0530

    code changes

---
+++ 6 (1).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter x value"<<endl;
+	cout<<showbase<<showpos<<uppercase;
+	cin>>oct>>x;
+	cout<<"x value= "<<x<<endl;
+	cout<<"x value= "<<hex<<x<<endl;
+	//cout<<"x value= "<<oct<<x<<endl;
+	cout<<"x value= "<<dec<<x<<endl;
+	//cout<<"x value= "<<x<<endl;
+}
